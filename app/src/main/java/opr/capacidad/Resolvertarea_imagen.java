5465034b95abc4f19ce92979fdd8925f81204fc1
SamuelRolonC <samuelrolonc@gmail.com>
2018-08-12 22:19:24 -0300

    mostrando imagenes bajadas del servidor

---
+++ app/src/main/java/opr/capacidad/Resolvertarea_imagen.java
@@ -0,0 +1,13 @@
+package opr.capacidad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Resolvertarea_imagen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resolvertarea_imagen);
+    }
+}
